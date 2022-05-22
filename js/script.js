cc162ec90d4d40cc8dce167b8f588e2316ce74ba
Serg Dev <info@devser.ru>
2022-05-22 21:17:26 +0300

    video3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-let a = 10;
-let b = 20;
-let c = a + b;
+/* let a = 10;
+let b = 5; */
+/* if(a > 0) {
+    if(b < a){
+    let c = (a + b) * 2;
+    alert(c);
+    }
+}  */
+/* if(a == b) {
+    alert('Равно');
+}
+else {
+    alert('Не равно');
+} */
+
+/* let count = 2;
+while (count <= 5) {
+    console.log(count);
+    count++;
+} */
+let c = 2;
+let i =0;
+
+do {
+    c = c * 2;
+    i++;
+}
+while (i < 4);
 console.log(c);
